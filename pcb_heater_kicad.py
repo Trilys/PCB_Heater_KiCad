c83941484e8102c36bf23e919a7232c9a7477e27
Pierre <p.lescure@trilys.com>
2018-07-06 10:13:20 +0200

    Works.

--- pcb_heater_kicad.py
+++ pcb_heater_kicad.py
@@ -10,6 +10,18 @@ import getopt
 import os
 #Used to create an array
 import numpy as np
+#For sqrt
+import math
+
+#Global vars :
+kicadFile = ""
+sizeX=10.0
+sizeY=10.0
+width=0.25
+length=0
+net=0
+space=0.5
+layer="F.Cu"
 
 def Help(out):
 	print os.path.basename(__file__) +' -x <size of X> -y <size of Y>'
@@ -18,23 +30,23 @@ def Help(out):
 	print("  which needs to be included in a .kicad_pcb file.")
 	sys.exit(out)
 
-def setCoord(x1, y1, x2, y2, width, layer, net, length):
+def setCoord(x1, y1, x2, y2):
+	global length
 	kicadFile.write("\n  (segment (start "+str(x1)+" "+str(y1)+") (end "+str(x2)+" "+str(y2)+") (width "+str(width)+") (layer "+str(layer)+") (net "+str(net)+"))")
-	length+=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+	length+=math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
 	return
 
 def main(argv):
-	sizeX=10.0
-	sizeY=10.0
-	width=0.25
-	length=0
-	net=0
-	space=0.5
-	layer="F.Cu"
+	global sizeX
+	global sizeY
+	global width
+	global length
+	global net
+	global space
+	global layer
+	global kicadFile
 	try:
 		opts, args = getopt.getopt(argv,"hx:y:X:Y:w:W:n:N:s:S:l:L",["help=", "width", "net", "space", "layer"])
-		print("opts="+str(opts)+"len="+str(len(opts)))
-		print("args="+str(args))
 	except getopt.GetoptError:
 		Help(2)
 	for opt, arg in opts:
@@ -70,12 +82,11 @@ def main(argv):
 				Help(2)
 		else:
 			Help(0)
-	print("X="+str(sizeX)+"mm")
-	print("Y="+str(sizeY)+"mm")
-	print("length="+str(length)+"mm")
-	print("width="+str(width)+"mm")
-	print("Space="+str(space)+"mm")
-	print("net="+str(net))
+	print("Size of X="+str(sizeX)+"mm")
+	print("Size of Y="+str(sizeY)+"mm")
+	print("width of wire="+str(width)+"mm")
+	print("Space between tracks="+str(space)+"mm")
+	print("net number="+str(net))
 	print("layer="+layer)
 	
 	#Create a new file kicad_pcb:
@@ -83,24 +94,26 @@ def main(argv):
 
 	for i in np.arange(0.0, sizeX, space):
 		#Top Left to top right
-		kicadFile.write("\n  (segment (start "+str(i-2*space)+" "+str(i)+") (end "+str(sizeX-i)+" "+str(i)+") (width "+str(width)+") (layer F.Cu) (net 0))")
-		length+=abs((sizeX-i)-(i-2*space))
+		if i<space:
+			setCoord(i, i, sizeX-i, i)
+		elif i<2*space:
+			setCoord(i-space, i, sizeX-i, i)
+		else:
+			setCoord(i-2*space, i, sizeX-i, i)
 		if (i+1>space+sizeX/2 or i+1>space+sizeY/2):
 			if sizeX<sizeY:
-				kicadFile.write("\n  (segment (start "+str(sizeX-i)+" "+str(i)+") (end "+str(i)+" "+str(sizeY-i)+") (width "+str(width)+") (layer F.Cu) (net 0))")
-				length+=sqrt((sizeX-2*i)*(sizeX-2*i)+(sizeY-2*i)*(sizeY-2*i))
-				kicadFile.write("\n  (segment (start "+str(i)+" "+str(sizeY-i)+") (end "+str(i-space)+" "+str(i-space+1)+") (width "+str(width)+") (layer F.Cu) (net 0))")
-				length+=sqrt((sizeX-2*i)*(sizeX-2*i)+(sizeY-2*i)*(sizeY-2*i))
+				setCoord(sizeX-i, i, i, sizeY-i)
+				setCoord(i, sizeY-i, i-space, i-space+1)
 			break
 		#Top right to bottom right
-		kicadFile.write("\n  (segment (start "+str(sizeX-i)+" "+str(i)+") (end "+str(sizeX-i)+" "+str(sizeY-i)+") (width "+str(width)+") (layer F.Cu) (net 0))")
+		setCoord(sizeX-i, i, sizeX-i, sizeY-i)
 		#Bottom right to bottom left
-		kicadFile.write("\n  (segment (start "+str(sizeX-i)+" "+str(sizeY-i)+") (end "+str(i)+" "+str(sizeY-i)+") (width "+str(width)+") (layer F.Cu) (net 0))")
+		setCoord(sizeX-i, sizeY-i, i, sizeY-i)
 		#Bottom left to top left
 		if (sizeY-i!=i+1):
-			kicadFile.write("\n  (segment (start "+str(i)+" "+str(sizeY-i)+") (end "+str(i)+" "+str(i+1)+") (width "+str(width)+") (layer F.Cu) (net 0))")
+			setCoord(i, sizeY-i, i, i+1)
 		else:
-			kicadFile.write("\n  (segment (start "+str(i)+" "+str(sizeY-i)+") (end "+str(sizeX-i-space)+" "+str(i+space)+") (width "+str(width)+") (layer F.Cu) (net 0))")
+			setCoord(i, sizeY-i, sizeX-i-space, i+space)
 	
 	return
 	
@@ -109,6 +122,8 @@ def main(argv):
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
+	print("---------------------------------------------")
+	print("Size of heater = "+str(length)+"mm.")
 	print("---------------------------------------------")
 	print("End of "+os.path.basename(__file__))
 
